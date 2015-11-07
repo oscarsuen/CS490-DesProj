79dfbcf4daeeca09ea61b0d72fcce215ec88afeb
oscarsuen <suen.oscar@gmail.com>
2015-11-07 10:30:27 -0500

    added search for country and statistic

---
+++ country.py
@@ -0,0 +1,26 @@
+import pymysql.cursors
+from tabulate import tabulate
+
+def getdata(sql):
+	co = pymysql.connect(host='localhost', user='root', db='econtest')
+	try:
+		with co.cursor() as cursor:
+			cursor.execute(sql)
+			rt = cursor.fetchall()
+	finally:
+		co.close()
+		return rt
+
+def sql(country):
+	rt = "SELECT `Country Code`, `Table Name`, `Long Name` FROM country WHERE "
+	rt = rt + "`Long Name` LIKE '%"+country+"%' OR "
+	rt = rt + "`Table Name` LIKE '%"+country+"%' OR "
+	rt = rt + "`Short Name` LIKE '%"+country+"%' OR "
+	rt = rt + "`Country Code` LIKE '%"+country+"%'"
+	return rt
+
+def run(query):
+	print(tabulate(getdata(sql(query)), headers=["Country Code", "Table Name", "Long Name"]))
+
+query = "U.S."
+run(query)
---
+++ stat.py
@@ -0,0 +1,23 @@
+import pymysql.cursors
+from tabulate import tabulate
+
+def getdata(sql):
+	co = pymysql.connect(host='localhost', user='root', db='econtest')
+	try:
+		with co.cursor() as cursor:
+			cursor.execute(sql)
+			rt = cursor.fetchall()
+	finally:
+		co.close()
+		return rt
+
+def sql(stat):
+	rt = "SELECT `Series Code`, `Topic`, `Indicator Name` FROM series WHERE `Indicator Name` LIKE '%"+stat+"%'"
+	return rt
+
+
+def run(query):
+	print(tabulate(getdata(sql(query)), headers=["Series Code", "Topic", "Indicator"]))
+
+query = "Foreign Direct Investment"
+run(query)
