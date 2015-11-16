1d0a259d891519c8d69b755ec58ba00323c9a08a
lefriedberg <lefriedberg@mail.ru>
2015-11-16 11:22:49 -0500

    gui stuff

--- GUI.py
+++ GUI.py
@@ -4,7 +4,7 @@ from tkinter import *
 root = Tk()
 
 logo = PhotoImage(file="graphs/image.gif")
-w1 = Label(master = root, image=logo).grid(row=0, column=0)
+w1 = Label(master = root, image=logo).grid(row=1, column=0)
 explanation = "Hello World"
 
 def test():
@@ -14,10 +14,12 @@ w2 = Button(master=root,
            justify=LEFT,
            padx = 10, 
            text=explanation,
-           command = test).grid(row=1, column=0)
+           command = test).grid(row=0, column=0)
 
-#searchbox = Tk()
+searchbox = Tk()
 
+csearch = Label(master = searchbox, text = "Search for a country").grid(row =1, column=1) #what is the command
+ssearch = Label(master = searchbox, text = "Search for a statistic" ).grid(row =2, column=1)
 
 
 root.mainloop()
