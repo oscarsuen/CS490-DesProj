cdbe0153d4f7ca2420fd11871d716fcf9276b738
lefriedberg <lefriedberg@mail.ru>
2015-11-16 11:13:49 -0500

    gui stuff

--- GUI.py
+++ GUI.py
@@ -4,11 +4,21 @@ from tkinter import *
 root = Tk()
 
 logo = PhotoImage(file="graphs/image.gif")
-w1 = Label(root, image=logo).pack(side="right")
+w1 = Label(master = root, image=logo).grid(row=0, column=0)
 explanation = "Hello World"
-w2 = Label(root, 
+
+def test():
+	print("hi")
+
+w2 = Button(master=root, 
            justify=LEFT,
            padx = 10, 
-           text=explanation).pack(side="left")
+           text=explanation,
+           command = test).grid(row=1, column=0)
+
+#searchbox = Tk()
+
+
+
 root.mainloop()
 
